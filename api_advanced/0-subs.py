7e08021cf519979fa1898a8172ac71b60322d78f
MohamedAYasin <m.yasin@alustudent.com>
2023-07-16 08:31:45 +0000

    api advanced

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Query the Reddit API to get the number of subscribers for a given subreddit
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Returns the number of subscribers for a given subreddit
+    """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    data = response.json().get('data')
+    return data.get('subscribers')
