c3e561fa621bf479054ff0e4f29c1efe309228de
Mohamed Ahmed Yasin <116743995+MohamedAYasin@users.noreply.github.com>
2023-07-16 13:37:30 +0300

    advanced api

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 """Write a recursive function that queries
- the Reddit API"""
+ the Reddit API, parses the title of all hot 
+ articles. """
 
 import requests
 
-
 def count_words(subreddit, word_list, after='', word_dict={}):
+    """ A function that queries the Reddit API parses the title of
+    all hot articles, and prints a sorted count of given keywords
+    (case-insensitive, delimited by spaces.
+    Javascript should count as javascript, but java should not).
+    If no posts match or the subreddit is invalid, it prints nothing.
+    """
 
     if not word_dict:
         for word in word_list:
