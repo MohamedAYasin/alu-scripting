c2bf63c304af0b04cf15669c93da579bbdfc7ffa
Mohamed Ahmed Yasin <116743995+MohamedAYasin@users.noreply.github.com>
2023-07-16 11:41:45 +0300

    Advanced API

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-2. Recurse it!
+Writing a recursive function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit.
+If no results are found for the given subreddit, the function returns None.
 """
 import requests
 
