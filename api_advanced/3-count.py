220e6e5ca110aa387d26e0e8d5aa019ba7885585
Mohamed Ahmed Yasin <116743995+MohamedAYasin@users.noreply.github.com>
2023-07-16 13:31:23 +0300

    Advanced APIs

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -9,8 +9,6 @@ def count_words(subreddit, word_list, after='', word_dict={}):
     """ A function that queries the Reddit API parses the title of
     all hot articles, and prints a sorted count of given keywords
     (case-insensitive, delimited by spaces.
-    Javascript should count as javascript, but java should not).
-    If no posts match or the subreddit is invalid, it prints nothing.
     """
 
     if not word_dict:
