95a24ce983f3a4bbf9e9438d78055b891385e59a
Mohamed Ahmed Yasin <116743995+MohamedAYasin@users.noreply.github.com>
2023-07-16 11:43:19 +0300

    Advanced APIs

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-""" 3-count.py """
+"""
+Writing a recursive function that queries the Reddit API, parses the title of all hot articles, and prints
+a sorted count of given keywords (case-insensitive, delimited by spaces. Javascript should count as javascript, 
+but java should not).
+"""
 import json
 import requests
 
