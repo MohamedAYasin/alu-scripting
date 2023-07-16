276fe9c51af1a910c4f4119754f896e9d5497e51
Mohamed Ahmed Yasin <116743995+MohamedAYasin@users.noreply.github.com>
2023-07-16 11:39:06 +0300

    advanced API

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Query the Reddit API to get the number of subscribers for a given subreddit
+A  function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) 
+for a given subreddit. If an invalid subreddit is given, the function returns 0.
 """
 
 import requests
